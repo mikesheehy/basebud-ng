9dfb52a300ea034b328455aa5e53e662e938abd0
Mike Sheehy <sheehy@outlook.com>
2024-12-28 22:02:18 -0500

    Listing and detail components

--- src/app/components/listing-details/listing-details.component.ts
+++ src/app/components/listing-details/listing-details.component.ts
@@ -1,10 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ListingService } from '../../services/listing.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Listing } from '../../models/listing.model';
 
 @Component({
   selector: 'app-listing-details',
   templateUrl: './listing-details.component.html',
   styleUrl: './listing-details.component.css'
 })
-export class ListingDetailsComponent {
+export class ListingDetailsComponent implements OnInit{
+
+  @Input() viewMode = false;
+
+  @Input() currentListing: Listing = {
+    title: '',
+    description: '',
+    published: false
+  };
+
+  message = '';
+
+  constructor(
+    private listingService: ListingService,
+    private route: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    if (!this.viewMode) {
+      this.message = '';
+      this.getListing(this.route.snapshot.params["id"]);
+    }
+  }
+
+  getListing(id: string): void {
+    this.listingService.get(id)
+      .subscribe({
+        next: (data) => {
+          this.currentListing = data;
+          console.log(data);
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  updatePublished(status: boolean): void {
+    const data = {
+      title: this.currentListing.title,
+      description: this.currentListing.description,
+      published: status
+    };
+
+    this.message = '';
+
+    this.listingService.update(this.currentListing.id, data)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.currentListing.published = status;
+          this.message = res.message ? res.message : 'The status was updated successfully!';
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  updateListing(): void {
+    this.message = '';
+
+    this.listingService.update(this.currentListing.id, this.currentListing)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.message = res.message ? res.message : 'This listing was updated successfully!';
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  deleteListing(): void {
+    this.listingService.delete(this.currentListing.id)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.router.navigate(['/listings']);
+        },
+        error: (e) => console.error(e)
+      });
+  }
 
 }
--- src/app/components/listings/listings.component.ts
+++ src/app/components/listings/listings.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Listing } from '../../models/listing.model';
+import { ListingService } from '../../services/listing.service';
 
 @Component({
   selector: 'app-listings',
   templateUrl: './listings.component.html',
   styleUrl: './listings.component.css'
 })
-export class ListingsComponent {
+export class ListingsComponent implements OnInit{
 
+  listings?: Listing[];
+  currentListing: Listing = {};
+  currentIndex = -1;
+  title = '';
+
+  constructor(private listingService: ListingService) { }
+
+  ngOnInit(): void {
+    this.retrieveListings();
+  }
+
+  retrieveListings(): void {
+    this.listingService.getAll()
+      .subscribe({
+        next: (data) => {
+          this.listings = data;
+          console.log(data);
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  refreshList(): void {
+    this.retrieveListings();
+    this.currentListing = {};
+    this.currentIndex = -1;
+  }
+
+  setActiveListing(listing: Listing, index: number): void {
+    this.currentListing = listing;
+    this.currentIndex = index;
+  }
+
+  removeAllListings(): void {
+    this.listingService.deleteAll()
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.refreshList();
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  searchTitle(): void {
+    this.currentListing = {};
+    this.currentIndex = -1;
+
+    this.listingService.findByTitle(this.title)
+      .subscribe({
+        next: (data) => {
+          this.listings = data;
+          console.log(data);
+        },
+        error: (e) => console.error(e)
+      });
+  }
 }
